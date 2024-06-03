c25827090f27ae0a3c510dd87b3d119c529a8887
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-06-03 17:55:33 +0530

    Update config.py

--- Youtube/config.py
+++ Youtube/config.py
@@ -1,4 +1,3 @@
-
 import os
 
 class Config(object):
@@ -8,5 +7,5 @@ class Config(object):
     CHANNEL = os.environ.get("CHANNEL", "-1002059087867")
     HTTP_PROXY = ''
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://t25821653:sUUQp5IhqoRDlwEj@cluster0.jrwf82r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "mydatabase")  # Add your database name here
     ADMIN_IDS = [1280494242, 6703267231]  # Add your admin user IDs here
-     
