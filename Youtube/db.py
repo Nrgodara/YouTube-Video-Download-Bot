e135c30ea1485d5558189d9a9a07567bcd037c61
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-06-03 12:34:02 +0530

    Update db.py

--- Youtube/db.py
+++ Youtube/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from Youtube.config import Config
 
-client = MongoClient(Config.MONGO_URL)
+client = MongoClient(Config.MONGO_URI)
 db = client[Config.DB_NAME]
 users_collection = db['users']
 
