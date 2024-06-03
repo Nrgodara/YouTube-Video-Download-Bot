eae4d5bbce78bc7f625b05b19aa5891040739b6b
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-06-03 11:36:36 +0530

    Create db.py

---
+++ Youtube/db.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+from Youtube.config import Config
+
+client = MongoClient(Config.MONGO_URI)
+db = client['youtube_bot']
+users_collection = db['users']
+
+def add_user(user_id):
+    if not users_collection.find_one({"user_id": user_id}):
+        users_collection.insert_one({"user_id": user_id})
+
+def get_all_users():
+    return users_collection.find()
+  
